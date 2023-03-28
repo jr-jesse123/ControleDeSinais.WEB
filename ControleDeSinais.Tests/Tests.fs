be1763a4ba4d9a9db6453859aea8d738827b914f
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2023-03-28 19:47:03 -0300

    entity framework setup working

--- ControleDeSinais.Tests/Tests.fs
+++ ControleDeSinais.Tests/Tests.fs
@@ -1,8 +1,32 @@
-module Tests
+module InfraEstruturaTests
 
 open System
 open Xunit
 
+open InfraEstrutura
+open Microsoft.EntityFrameworkCore
+
 [<Fact>]
-let ``My test`` () =
+let ``Posso salva e recuperar um registro no banco de dados`` () =
+    let options = 
+        DbContextOptionsBuilder<BloggingContext>()
+            //.UseSqlite("DataSource=:memory:")
+            .UseSqlite("Data Source=blogging.db")
+            .Options
+    
+    use context = new BloggingContext(options)
+
+    context.Database.EnsureDeleted() |> ignore
+
+    context.Database.EnsureCreated() |> ignore
+
+    let blog = { Id = 1 ; Url = "http://sample.com" }
+
+    context.Blogs.Add(blog) |> ignore
+
+    context.SaveChanges() |> ignore
+
+    context.Blogs.ToListAsync().Result |> Seq.last |> fun x -> Assert.Equal(blog,x)
+
     Assert.True(true)
+
--- ControleDeSinais.WEB/App/InfraEstrutura.fs
+++ ControleDeSinais.WEB/App/InfraEstrutura.fs
@@ -7,6 +7,7 @@ open Interfaces
 open System.ComponentModel.DataAnnotations
 open Microsoft.EntityFrameworkCore
 open EntityFrameworkCore.FSharp.Extensions
+open Microsoft.EntityFrameworkCore.Design
 
 [<CLIMutable>]
 type Blog = {
@@ -16,18 +17,37 @@ type Blog = {
 
 
 
-type BloggingContext() =  
-    inherit DbContext()
+type BloggingContext(options) =  
+    inherit DbContext(options)
     
-    [<DefaultValue>] val mutable blogs : DbSet<Blog>
+    [<DefaultValue>]  val mutable private blogs : DbSet<Blog>
     member this.Blogs with get() = this.blogs and set v = this.blogs <- v
 
     override _.OnModelCreating builder =
         builder.RegisterOptionTypes() // enables option values for all entities
+        
 
-    override __.OnConfiguring(options: DbContextOptionsBuilder) : unit =
-        options.UseSqlite("Data Source=blogging.db") |> ignore
+    //override __.OnConfiguring(options: DbContextOptionsBuilder) : unit =
+        //options.UseSqlite("Data Source=blogging.db") |> ignore
 
+type DesignTimeDbContextFactory() =
+        interface IDesignTimeDbContextFactory<BloggingContext> with
+            member __.CreateDbContext(args) =
+                let optionsBuilder = DbContextOptionsBuilder<BloggingContext>()
+                optionsBuilder.UseSqlite("Data Source=blogging.db") |> ignore
+                new BloggingContext(optionsBuilder.Options)
 
 
 //TODO: CREATE CONFIGURATION FILES TO INCLUDE FIXED ENTITIES
+
+
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.EntityFrameworkCore.Design
+open EntityFrameworkCore.FSharp
+
+type DesignTimeServices() =
+    interface IDesignTimeServices with 
+        member __.ConfigureDesignTimeServices(serviceCollection: IServiceCollection) = 
+            let fSharpServices = EFCoreFSharpServices.Default
+            fSharpServices.ConfigureDesignTimeServices serviceCollection
+            ()
