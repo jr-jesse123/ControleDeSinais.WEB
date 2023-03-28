45030e62fa0105b97ed8b30272064efa38adea22
jr-jesse123 <jesse@quatrodconsultoria.com.br>
2023-03-27 21:15:37 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeSinais.WEB/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace ControleDeSinais.WEB.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
